6072dad21fd410a1687e574f525fcb525d0887e9
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2026-03-13 11:11:30 -0600

    Remove redundant order property from specials and update header text for clarity

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -12,7 +12,6 @@ function Specials() {
 			price: "$12.99",
 			description:
 				"The famous Greek salad of crispy lettuce, peppers, olives and our Chicago styled feta cheese, garnished with crunchy garlic, rosemary croutons.",
-			order: "Order a delivery",
 		},
 		{
 			image: Item2,
@@ -20,7 +19,6 @@ function Specials() {
 			price: "$5.99",
 			description:
 				"Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil; made perfect for an evening dinner.",
-			order: "Order a delivery",
 		},
 		{
 			image: Item3,
@@ -28,14 +26,13 @@ function Specials() {
 			price: "$5.00",
 			description:
 				"This comes straight from Grandma's recipe book. Every last ingredient has been sourced and is as authentic as can be imagined.",
-			order: "Order a delivery",
 		},
 	];
 
 	return (
 		<section id="menu">
 			<div className="specials-header">
-				<h2>This weeks specials</h2>
+				<h2>This weeks specials!</h2>
 				<button className="button">Online Menu</button>
 			</div>
 			<div className="specials-grid">
