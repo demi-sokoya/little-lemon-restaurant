46cb377a9adea6dd5acd969740e23b1c9e6f02b1
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2026-03-12 21:45:13 -0600

    Add Text and Button to Hero content, style hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,20 @@
+import "./Styles/layout.css";
+
 function Hero() {
-	return <section></section>;
+	return (
+		<section className="hero">
+			<div>
+				<h1>Little Lemon</h1>
+				<h2>Chicago</h2>
+				<p>
+					We are a family owned Mediterranean restaurant, focused on traditional
+					recipes served with a modern twist.
+				</p>
+				<button>Reserve a table</button>
+			</div>
+			<img src="" alt="Chef presenting a platter of delicious food" />
+		</section>
+	);
 }
 
 export default Hero;
