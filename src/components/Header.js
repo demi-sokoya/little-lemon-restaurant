a7f1b7b7cd2ddfe2d364f04d984db6db19e2526b
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2026-03-12 19:18:14 -0600

    Create structure for Header element and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,30 @@
+import littleLemonLogo from "./Assets/Logo.svg";
 function Header() {
 	return (
 		<header>
-			<image>Logo</image>
-			<nav>links</nav>
+			<img src={littleLemonLogo} className="logo" alt="Little Lemon Logo" />
+			<nav>
+				<ul>
+					<li>
+						<a href="">Home</a>
+					</li>
+					<li>
+						<a href="">About</a>
+					</li>
+					<li>
+						<a href="">Menu</a>
+					</li>
+					<li>
+						<a href="">Reservations</a>
+					</li>
+					<li>
+						<a href="">Order Online</a>
+					</li>
+					<li>
+						<a href="">Login</a>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 }
