987ebbac654d3105ed6ea1eebfc4ddbc69794095
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2026-03-12 21:58:30 -0600

    Add placeholder content for specials and style section

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,5 +1,19 @@
+import "./Styles/layout.css";
+
 function Specials() {
-	return <section></section>;
+	return (
+		<section>
+			<div className="specials-header">
+				<h2>This weeks specials</h2>
+				<button>Online Menu</button>
+			</div>
+			<div className="specials-grid">
+				<div>Card 1</div>
+				<div>Card 2</div>
+				<div>Card 3</div>
+			</div>
+		</section>
+	);
 }
 
 export default Specials;
