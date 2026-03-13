4c080e007aaf89fef280e4c580ed4b30cebafbc2
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2026-03-13 13:10:43 -0600

    Add About section styles with responsive design and image hover effects

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,29 @@
-import "./Styles/layout.css";
+import "./Styles/About.css";
+import AboutImage1 from "./Assets/Mario-and-Adrian.jpg";
+import AboutImage2 from "./Assets/restaurant-interior.jpg";
 
 function About() {
 	return (
-		<section className="about" id="about">
-			<div>
+		<section className="about-section" id="about">
+			<div className="about-text">
 				<h2>Little Lemon</h2>
 				<h3>Chicago</h3>
-				<p>Placeholder about text...</p>
+				<p>
+					Little Lemon Chicago is a family owned Mediterranean restaurant
+					located in the heart of the city. The restaurant is run by brothers
+					Mario and Adrian, who have always had a passion for cooking and
+					serving delicious food. Growing up in a Mediterranean household, the
+					brothers were exposed to traditional recipes from an early age, and
+					they decided to bring those recipes to the masses with a modern twist.
+				</p>
 			</div>
 			<div className="about-images">
-				<img src="" alt="restaurant" />
-				<img src="" alt="restaurant" />
+				<img className="img-top" src={AboutImage1} alt="Mario and Adrian" />
+				<img
+					className="img-bottom"
+					src={AboutImage2}
+					alt="Restaurant Interior"
+				/>
 			</div>
 		</section>
 	);
