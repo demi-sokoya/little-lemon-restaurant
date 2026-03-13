94bf6eff015a43c9a951ac9a900827c3bc54114e
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2026-03-13 09:47:33 -0600

    Refactor Hero component layout with flexbox; add responsive styles and new CSS for sections

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,30 @@
 import { Link } from "react-router-dom";
 import "./Styles/layout.css";
+import "./Styles/Hero.css";
 import heroImage from "./Assets/heroImage.jpg";
 
 function Hero() {
 	return (
 		<section className="hero" id="home">
-			<div>
-				<h1>Little Lemon</h1>
-				<h2>Chicago</h2>
-				<p>
-					We are a family owned Mediterranean restaurant, focused on traditional
-					recipes served with a modern twist.
-				</p>
-				<Link to="/booking" className="button">
-					Reserve a table
-				</Link>
+			<div className="hero-section-container">
+				<div className="left-section">
+					<h1>Little Lemon</h1>
+					<h2>Chicago</h2>
+					<p>
+						We are a family owned Mediterranean restaurant, focused on
+						traditional recipes served with a modern twist.
+					</p>
+					<Link to="/booking" className="button">
+						Reserve a table
+					</Link>
+				</div>
+				<div className="right-section">
+					<img
+						src={heroImage}
+						alt="Chef presenting a platter of delicious food"
+					/>
+				</div>
 			</div>
-			<img src={heroImage} alt="Chef presenting a platter of delicious food" />
 		</section>
 	);
 }
