87a1018e5816e31f1af4befb7e9cd7ca233daadf
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2026-03-22 13:55:53 -0600

    Update routes for github pages

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 			<Header />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/little-lemon-restaurant" element={<HomePage />} />
 				<Route path="/booking" element={<BookingPage />} />
 				<Route path="/confirmed" element={<ConfirmedBooking />} />
 			</Routes>
