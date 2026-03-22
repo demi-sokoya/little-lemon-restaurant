0417339174a67d4bad2049de448cbc383a274ef6
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2026-03-22 16:33:31 -0600

    Remove github pages

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import BookingPage from "./components/BookingPage";
 import HomePage from "./components/Homepage";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ConfirmedBooking from "./components/ConfirmedBooking";
 
 function App() {
 	return (
-		<BrowserRouter basename="/little-lemon-restaurant">
+		<>
 			<Header />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
@@ -18,7 +18,7 @@ function App() {
 				<Route path="/confirmed" element={<ConfirmedBooking />} />
 			</Routes>
 			<Footer />
-		</BrowserRouter>
+		</>
 	);
 }
 export default App;
